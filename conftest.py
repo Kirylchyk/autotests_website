2ed8f7a64de6171ed099b84ccc032f05794e14fa
varvara <kirylchyk@gmail.com>
2022-10-03 15:16:12 -0400

    run headless 1

--- conftest.py
+++ conftest.py
@@ -8,6 +8,9 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope='session')
 def driver():
     chrome_options = Options()
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox") # linux only
     chrome_options.add_argument("--headless")
     chrome_driver = webdriver.Chrome(options=chrome_options)
     chrome_driver.maximize_window()
